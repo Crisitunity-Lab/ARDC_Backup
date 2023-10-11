6a6e5b5fdeb6b5eb6091c68365bf9c189d9cf957
Will Kent <wjokent@gmail.com>
2023-10-11 20:44:00 +1100

    Add error messages

--- src/structure_extractor/data_utils.py
+++ src/structure_extractor/data_utils.py
@@ -12,7 +12,7 @@ def unzip_from_url(src, dest="./"):
     zip.extractall(dest)
     print("Download and Unzip complete")
 
-def combine_csv_files(folder_path, retrieve_label=True, retrieve_country=True):
+def combine_csv_files(folder_path, retrieve_label=True, retrieve_year=True, retrieve_country=True):
     df = pd.DataFrame()
     file_suffix=".csv"
 
@@ -29,8 +29,18 @@ def combine_csv_files(folder_path, retrieve_label=True, retrieve_country=True):
                     label=os.path.basename(os.path.dirname(file_path))
                     csv_df["label"]=label
                 
+                # Get the year of the event from the label
+                if retrieve_year:
+                    if retrieve_label:
+                        csv_df["year"]=csv_df["label"].str[0:4]
+                    else:
+                        raise Exception("Label needs to be retrieved for year to be found")
+
                 if retrieve_country:
-                    csv_df=csv_df.assign(country_code=lambda x: _get_country_of_crisis(csv_df["label"]))
+                    if retrieve_label:
+                        csv_df=csv_df.assign(country_code=lambda x: _get_country_of_crisis(csv_df["label"]))
+                    else:
+                        raise Exception("Label needs to be retrieved for Country to be found")
                 
                 # Concatenate new csv data to data frame
                 df=pd.concat([df, csv_df])
